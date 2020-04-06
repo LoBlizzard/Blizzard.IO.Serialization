f0e2933217c112f17b4b98ff3a5ab10068f5ab68
alloncm <alloncm@gmail.com>
2020-04-06 18:30:39 +0300

    added json serialzier for rpc

---
+++ Blizzard.IO.Serialization/Rpc/JsonSerializer.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Blizzard.IO.Core.Rpc;
+using System;
+using System.Text;
+
+namespace Blizzard.IO.Serialization.Rpc
+{
+    public class JsonSerializer : ISerializer
+    {
+        public byte[] Serialize(object data)
+        {
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data));
+        }
+
+        public object Deserialize(byte[] data, Type type)
+        {
+            return JsonConvert.DeserializeObject(Encoding.UTF8.GetString(data), type);
+        }
+    }
+}
